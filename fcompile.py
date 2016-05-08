a9de19c9126124481acf9a08013b7fae9ddb122f
Jan Hermann <jh@janhermann.cz>
2016-05-08 13:53:26 +0200

    change api of json input

--- fcompile.py
+++ fcompile.py
@@ -332,7 +332,7 @@ if __name__ == '__main__':
     parser.add_option('-j', '--jobs', type='int', default=cpu_count(), help='number of threads')
     parser.add_option('--dry', action='store_true', help='print changed files and exit')
     opts, _ = parser.parse_args()
-    tasks = json.load(sys.stdin)['fcompile']
+    tasks = json.load(sys.stdin)
     try:
         with timing('all'):
             sys.exit(build(tasks, opts))
